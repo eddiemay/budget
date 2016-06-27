0fbedeb844acf3dab0d681f58c6ed8248d8365f6
Eddie Mayfield <eddiemay1999@yahoo.com>
2016-06-26 19:55:33 -0700

    Account Store Impl

---
+++ src/com/digitald4/budget/store/AccountStore.java
@@ -0,0 +1,20 @@
+package com.digitald4.budget.store;
+
+import com.digitald4.budget.proto.BudgetProtos.Account;
+import com.digitald4.common.dao.DAO;
+import com.digitald4.common.dao.QueryParam;
+import com.digitald4.common.exception.DD4StorageException;
+import com.digitald4.common.store.impl.GenericDAOStore;
+
+import java.util.List;
+
+public class AccountStore extends GenericDAOStore<Account> {
+	
+	public AccountStore(DAO<Account> dao) {
+		super(dao);
+	}
+	
+	public List<Account> getByPortfolio(int portfolioId) throws DD4StorageException {
+		return query(new QueryParam("portfolio_id", "=", portfolioId));
+	}
+}
