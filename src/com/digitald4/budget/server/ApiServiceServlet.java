92c165ce47cb3de2ee5edfde694b08072d3cc022
eddiemay <eddiemay1999@yahoo.com>
2017-03-13 21:03:44 -0700

    Added Generated Proto Java files, added ApiServiceServlet, got rid of generated proto js files.

---
+++ src/com/digitald4/budget/server/ApiServiceServlet.java
@@ -0,0 +1,35 @@
+package com.digitald4.budget.server;
+
+import com.digitald4.budget.proto.BudgetProtos.Account;
+import com.digitald4.budget.proto.BudgetProtos.Bill;
+import com.digitald4.budget.proto.BudgetProtos.Template;
+import com.digitald4.budget.storage.*;
+import com.digitald4.common.jdbc.DBConnector;
+import com.digitald4.common.storage.DAOProtoSQLImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "API Service Servlet", urlPatterns = {"/api/*"})
+public class ApiServiceServlet extends com.digitald4.common.server.ApiServiceServlet {
+
+	public void init() throws ServletException {
+		super.init();
+		DBConnector dbConnector = getDBConnector();
+
+		PortfolioStore portfolioStore = new PortfolioStore(new PortfolioSQLDao(dbConnector));
+		addService("portfolio", new PortfolioService(portfolioStore, userProvider));
+
+		AccountStore accountStore = new AccountStore(
+				new DAOProtoSQLImpl<>(Account.class, dbConnector));
+		addService("account", new AccountService(accountStore));
+		
+		TemplateStore templateStore = new TemplateStore(
+				new DAOProtoSQLImpl<>(Template.class, dbConnector));
+		addService("template", new TemplateService(templateStore));
+		
+		BillStore billStore = new BillStore(
+				new DAOProtoSQLImpl<>(Bill.class, dbConnector), accountStore);
+		addService("bill", new BillService(billStore, templateStore));
+	}
+}
