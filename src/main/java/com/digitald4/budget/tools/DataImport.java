b5de4ac73187ecf06e3479f726d72833843d48e3
eddiemay <eddiemay1999@yahoo.com>
2017-09-16 08:26:24 -0700

    DataImport class and clean up of proto files.

---
+++ src/main/java/com/digitald4/budget/tools/DataImport.java
@@ -0,0 +1,18 @@
+package com.digitald4.budget.tools;
+
+import com.digitald4.budget.proto.BudgetProtos.Portfolio;
+import com.digitald4.common.jdbc.DBConnectorThreadPoolImpl;
+import com.digitald4.common.storage.DataConnectorSQLImpl;
+import com.digitald4.common.tools.DataImporter;
+
+public class DataImport {
+	public static void main(String[] args) throws Exception {
+		DataImporter importer = new DataImporter(
+				new DataConnectorSQLImpl(new DBConnectorThreadPoolImpl("org.gjt.mm.mysql.Driver",
+						"jdbc:mysql://localhost/budget?autoReconnect=true",
+						"dd4_user", "getSchooled85")),
+				"https://ledger-178106.appspot.com/api");
+		importer.login();
+		importer.runFor(Portfolio.class);
+	}
+}
